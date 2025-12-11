83cc0402555586508d4632f2d9fd68e341b81c67
Preston Ong <preston4896@me.com>
2025-12-11 18:24:54 +0800

    upgraded risczero to v3 and enabled deterministic builds for risczero and sp1

--- crates/risc0-methods/build.rs
+++ crates/risc0-methods/build.rs
@@ -1,3 +1,18 @@
+use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptionsBuilder};
+use std::collections::HashMap;
+use std::path::PathBuf;
+
 fn main() {
-    risc0_build::embed_methods();
+    let manifest_dir = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let mut builder = GuestOptionsBuilder::default();
+    if std::env::var("USE_DOCKER").is_ok() {
+        let docker_options = DockerOptionsBuilder::default()
+            .root_dir(manifest_dir.join("../../"))
+            .build()
+            .unwrap();
+
+        builder.use_docker(docker_options);
+    }
+    let guest_options = builder.build().unwrap();
+    embed_methods_with_options(HashMap::from([("risc0-verifier", guest_options)]));
 }
--- crates/sp1-methods/build.rs
+++ crates/sp1-methods/build.rs
@@ -1,3 +1,13 @@
+use sp1_build::{build_program_with_args, BuildArgs};
+
 fn main() {
-    sp1_build::build_program_with_args("../sp1-methods/sp1-verifier", Default::default());
+    build_program_with_args(
+        "./sp1-verifier",
+        BuildArgs {
+            output_directory: Some("./elf".to_string()),
+            elf_name: Some("nitro-sp1-guest-program-elf".to_string()),
+            docker: std::env::var("USE_DOCKER").is_ok(),
+            ..Default::default()
+        },
+    )
 }
