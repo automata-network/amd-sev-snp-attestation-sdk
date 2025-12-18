21c3d91fc4039b6a6373d9d24caaf39c3d795cd0
Preston Ong <preston4896@me.com>
2025-12-18 15:36:23 +0800

    sevsnptype contract update

--- contracts/src/types/SevSnpTypes.sol
+++ contracts/src/types/SevSnpTypes.sol
@@ -14,10 +14,14 @@ enum CertType {
     ARK
 }
 
+/// @notice TCB_VERSION structure can be found in Table 3 of the manual
+/// @notice https://www.amd.com/content/dam/amd/en/documents/epyc-technical-docs/specifications/56860.pdf
 struct TcbVersion {
+    uint8 fmc;
     uint8 bootloader;
     uint8 tee;
     uint8 snp;
+    uint8[3] reserved;
     uint8 microcode;
 }
 
@@ -63,10 +67,17 @@ library TcbVersionLib {
     using BytesUtils for bytes;
 
     function parseTcbVersion(bytes memory rawTcb) internal pure returns (TcbVersion memory tcb) {
+        uint8[3] memory reserved;
+        for (uint256 i = 0; i < 3; i++) {
+            reserved[i] = rawTcb.readUint8(4 + i);
+        }
+        
         tcb = TcbVersion({
-            bootloader: rawTcb.readUint8(0),
-            tee: rawTcb.readUint8(1),
-            snp: rawTcb.readUint8(6),
+            fmc: rawTcb.readUint8(0),
+            bootloader: rawTcb.readUint8(1),
+            tee: rawTcb.readUint8(2),
+            snp: rawTcb.readUint8(3),
+            reserved: reserved,
             microcode: rawTcb.readUint8(7)
         });
     }
@@ -75,14 +86,79 @@ library TcbVersionLib {
 library AttestationReportLib {
     using BytesUtils for bytes;
 
-    /// @notice Attestation Report Serialization can be found in Table 22 of the manual
+    /// @notice Attestation Report Serialization can be found in Table 23 of the manual
     /// @notice https://www.amd.com/content/dam/amd/en/documents/epyc-technical-docs/specifications/56860.pdf
     function deserializeRawAttestationReport(bytes memory attestatonReportRaw)
         internal
         pure
         returns (AttestationReport memory report)
     {
-        // TODO
+        // 0x00: version (4 bytes)
+        report.version = attestatonReportRaw.readUint32(0x00);
+        // 0x04: guest_svn (4 bytes)
+        report.guestSvn = attestatonReportRaw.readUint32(0x04);
+        // 0x08: policy (8 bytes)
+        report.guestPolicyRaw = bytes8(attestatonReportRaw.readBytesN(0x08, 8));
+        // 0x10: family_id (16 bytes)
+        report.familyId = bytes16(attestatonReportRaw.readBytesN(0x10, 16));
+        // 0x20: image_id (16 bytes)
+        report.imageId = bytes16(attestatonReportRaw.readBytesN(0x20, 16));
+        // 0x30: vmpl (4 bytes)
+        report.vmpl = attestatonReportRaw.readUint32(0x30);
+        // 0x34: sig_algo (4 bytes)
+        report.sigAlgo = attestatonReportRaw.readUint32(0x34);
+        // 0x38: current_tcb (8 bytes)
+        report.currentTcb = TcbVersionLib.parseTcbVersion(attestatonReportRaw.substring(0x38, TCB_VERSION_SIZE));
+        // 0x40: platform_info (8 bytes)
+        report.platInfoRaw = bytes8(attestatonReportRaw.readBytesN(0x40, 8));
+        // 0x48: author_key_en (4 bytes)
+        report.authorKeyEn = attestatonReportRaw.readUint32(0x48);
+        // 0x4C: reserved0 (4 bytes)
+        report.reserved0 = attestatonReportRaw.readUint32(0x4C);
+        // 0x50: report_data (64 bytes)
+        report.reportData = attestatonReportRaw.substring(0x50, 64);
+        // 0x90: measurement (48 bytes)
+        report.measurement = attestatonReportRaw.substring(0x90, 48);
+        // 0xC0: host_data (32 bytes)
+        report.hostData = attestatonReportRaw.readBytes32(0xC0);
+        // 0xE0: id_key_digest (48 bytes)
+        report.idKeyDigest = attestatonReportRaw.substring(0xE0, 48);
+        // 0x110: author_key_digest (48 bytes)
+        report.authorKeyDigest = attestatonReportRaw.substring(0x110, 48);
+        // 0x140: report_id (32 bytes)
+        report.reportId = attestatonReportRaw.readBytes32(0x140);
+        // 0x160: report_id_ma (32 bytes)
+        report.reportIdMd = attestatonReportRaw.readBytes32(0x160);
+        // 0x180: reported_tcb (8 bytes)
+        report.reportedTcb = TcbVersionLib.parseTcbVersion(attestatonReportRaw.substring(0x180, TCB_VERSION_SIZE));
+        // 0x188: reserved1 (24 bytes)
+        report.reserved1 = bytes24(attestatonReportRaw.readBytesN(0x188, 24));
+        // 0x1A0: chip_id (64 bytes)
+        report.chipId = attestatonReportRaw.substring(0x1A0, 64);
+        // 0x1E0: committed_tcb (8 bytes)
+        report.committedTcb = TcbVersionLib.parseTcbVersion(attestatonReportRaw.substring(0x1E0, TCB_VERSION_SIZE));
+        // 0x1E8: current_build (1 byte)
+        report.currentBuild = attestatonReportRaw.readUint8(0x1E8);
+        // 0x1E9: current_minor (1 byte)
+        report.currentMinor = attestatonReportRaw.readUint8(0x1E9);
+        // 0x1EA: current_major (1 byte)
+        report.currentMajor = attestatonReportRaw.readUint8(0x1EA);
+        // 0x1EB: reserved2 (1 byte)
+        report.reserved2 = attestatonReportRaw.readUint8(0x1EB);
+        // 0x1EC: committed_build (1 byte)
+        report.committedBuild = attestatonReportRaw.readUint8(0x1EC);
+        // 0x1ED: committed_minor (1 byte)
+        report.committedMinor = attestatonReportRaw.readUint8(0x1ED);
+        // 0x1EE: committed_major (1 byte)
+        report.committedMajor = attestatonReportRaw.readUint8(0x1EE);
+        // 0x1EF: reserved3 (1 byte)
+        report.reserved3 = attestatonReportRaw.readUint8(0x1EF);
+        // 0x1F0: launch_tcb (8 bytes)
+        report.launchTcb = TcbVersionLib.parseTcbVersion(attestatonReportRaw.substring(0x1F0, TCB_VERSION_SIZE));
+        // 0x1F8: reserved4 (168 bytes)
+        report.reserved_4 = attestatonReportRaw.substring(0x1F8, 168);
+        // 0x2A0: signature (512 bytes)
+        report.rawSignature = attestatonReportRaw.substring(0x2A0, 512);
     }
 
     /// @notice extract the reported tcb values from the report without parsing the entire data
